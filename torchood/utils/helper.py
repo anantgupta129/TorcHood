ee43171e5d1c0194b0d81c02763c1e6114c83f25
Anant Gupta <anantgupta129@gmail.com>
2023-07-31 12:10:15 +0530

    trainer

---
+++ torchood/utils/helper.py
@@ -0,0 +1,37 @@
+import os
+from typing import Any, Tuple
+
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import (LearningRateFinder,
+                                         LearningRateMonitor, RichModelSummary,
+                                         RichProgressBar)
+
+
+def auto_find_lr_and_fit(
+    model: pl.LightningModule,
+    datamodule: pl.LightningDataModule,
+    lr_finder_args: dict,
+    trainer_args: dict,
+    save_traced_model: bool = False,
+    input_shape: tuple = (1, 3, 32, 32),
+) -> Any:
+    callbacks = [
+        RichModelSummary(),
+        RichProgressBar(leave=True),
+        LearningRateFinder(**lr_finder_args),
+        LearningRateMonitor(logging_interval="step"),
+    ]
+
+    trainer = pl.Trainer(callbacks=callbacks, **trainer_args)
+    trainer.fit(model=model, datamodule=datamodule)
+
+    model = trainer.model
+    if save_traced_model:
+        model.to_torchscript(
+            method="trace", example_inputs=torch.rand(input_shape), file_path="model.traced.pt"
+        )
+        cwd = os.getcwd()
+        print(f" [x] Traced model saved at {cwd}/model.traced.pt")
+
+    return model, trainer

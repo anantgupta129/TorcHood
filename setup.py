4125bbd0d58370f112fc5b68c3d73520347ed2cd
Anant Gupta <anantgupta129@gmail.com>
2023-08-04 10:25:27 +0530

    proj setup

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 from setuptools import find_packages, setup
 
-VERSION = "0.1"
-DESCRIPTION = "A Pytorch Wrapper"
+VERSION = "0.2.0"
+DESCRIPTION = "A Pytorch & lightning  Wrapper, for rapid prototyping"
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
 
 setup(
     name="torchood",
     version=VERSION,
-    packages=find_packages(),
-    author="[Anant Gupta]",
+    author="Anant Gupta",
     author_email="anantgupta129@gmail.com",
-    install_requires=[
-        "torch==2.0.1",
-        "torchvision==0.15.2",
-        "lightning=2.0.6",
-        "albumentations==1.3.1",
-        "torch-lr-finder",
-        "torchinfo",
-        "grad-cam",
-        "torchmetrics==0.8.0",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anantgupta129/TorcHood",
+    packages=find_packages(exclude=["*tutorials*", "*notebooks*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.8",
+    install_requires=requirements,
 )

a6ba935c2dd7314e530275d24748dcbc6f1019e2
Anant Gupta <anantgupta129@gmail.com>
2023-08-09 11:15:00 +0530

    plot yolo

--- torchood/utils/plotting.py
+++ torchood/utils/plotting.py
@@ -1,12 +1,16 @@
+import io
 from random import randint
 from typing import Any
 
 import albumentations as A
+import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+
+from .box_utils import cells_to_bboxes, non_max_suppression
 
 
 def plot_sampledata(loader: DataLoader):
@@ -85,3 +89,89 @@ def plot_misclassified(
 
     if return_imgs:
         return imgs_list
+
+
+def plot_image(image, boxes, class_labels):
+    """Plots predicted bounding boxes on the image."""
+    cmap = plt.get_cmap("tab20b")
+    colors = [cmap(i) for i in np.linspace(0, 1, len(class_labels))]
+    im = np.array(image)
+    height, width, _ = im.shape
+
+    buffer = io.BytesIO()
+    # Create figure and axes
+    fig, ax = plt.subplots(1)
+    # Display the image
+    ax.imshow(im)
+
+    # box[0] is x midpoint, box[2] is width
+    # box[1] is y midpoint, box[3] is height
+
+    # Create a Rectangle patch
+    for box in boxes:
+        assert len(box) == 6, "box should contain class pred, confidence, x, y, width, height"
+        class_pred = box[0]
+        box = box[2:]
+        upper_left_x = box[0] - box[2] / 2
+        upper_left_y = box[1] - box[3] / 2
+        rect = patches.Rectangle(
+            (upper_left_x * width, upper_left_y * height),
+            box[2] * width,
+            box[3] * height,
+            linewidth=2,
+            edgecolor=colors[int(class_pred)],
+            facecolor="none",
+        )
+        # Add the patch to the Axes
+        ax.add_patch(rect)
+        plt.text(
+            upper_left_x * width,
+            upper_left_y * height,
+            s=class_labels[int(class_pred)],
+            color="white",
+            verticalalignment="top",
+            bbox={"color": colors[int(class_pred)], "pad": 0},
+        )
+
+    # Save the figure to the buffer in png format
+    fig.savefig(buffer, format="png")
+    # Seek back to the beginning of the buffer
+    buffer.seek(0)
+
+    # Read the buffer into a NumPy array
+    image = np.frombuffer(buffer.getvalue(), dtype=np.uint8)
+
+    # Reshape the image to match the dimensions of the figure
+    width, height, _ = fig.canvas.get_width_height()
+    image = image.reshape(height, width, 4)  # 4 channels (png)
+
+    return image
+
+
+def plot_couple_examples(model, loader, thresh, iou_thresh, anchors, class_labels):
+    x, y = next(iter(loader))
+    x = x.to("cuda")
+    with torch.no_grad():
+        out = model(x)
+        bboxes = [[] for _ in range(x.shape[0])]
+        for i in range(3):
+            batch_size, A, S, _, _ = out[i].shape
+            anchor = anchors[i]
+            boxes_scale_i = cells_to_bboxes(out[i], anchor, S=S, is_preds=True)
+            for idx, (box) in enumerate(boxes_scale_i):
+                bboxes[idx] += box
+
+        # model.train()
+
+    plotted_images = []
+    for i in range(batch_size // 4):
+        nms_boxes = non_max_suppression(
+            bboxes[i],
+            iou_threshold=iou_thresh,
+            threshold=thresh,
+            box_format="midpoint",
+        )
+        image = plot_image(x[i].permute(1, 2, 0).detach().cpu(), nms_boxes, class_labels)
+        plotted_images.append(image)
+
+    return plotted_images

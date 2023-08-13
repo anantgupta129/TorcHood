3dd47104e90caaf4a2632fefba1c3b859bf0e39d
Anant Gupta <anantgupta129@gmail.com>
2023-08-13 16:24:18 +0530

    remove redundant code

--- torchood/models/yolo_module.py
+++ torchood/models/yolo_module.py
@@ -16,22 +16,6 @@ from .components.yolo.loss import YoloLoss
 from .components.yolo.yolov3 import YOLOv3
 
 
-# class Metric:
-#     def __init__(self) -> None:
-#         self._load()
-
-#     def _load(self) -> None:
-#         self.losses = []
-#         self.tot_class_preds, self.correct_class = 0, 0
-#         self.tot_noobj, self.correct_noobj = 0, 0
-#         self.tot_obj, self.correct_obj = 0, 0
-#         self.all_pred_boxes = []
-#         self.all_true_boxes = []
-
-#     def reset(self) -> None:
-#         self._load()
-
-
 class YOLOv3LitModule(LightningModule):
     def __init__(
         self, learning_rate: float, config: Any, class_labels: List[str], in_channels: int = 3
@@ -54,8 +38,6 @@ class YOLOv3LitModule(LightningModule):
         self.learning_rate = learning_rate
 
         self.loss_fn = YoloLoss()
-        # self.train_metric = Metric()
-        # self.val_metric = Metric()
         self.train_loss = []
         self.val_loss = []
 
@@ -81,7 +63,6 @@ class YOLOv3LitModule(LightningModule):
         lr_scheduler = {"scheduler": scheduler, "interval": "step"}
         return [optimizer], [lr_scheduler]
 
-    # def _step(self, batch: Any, metric: Union[Metric, Any]) -> torch.Tensor:
     def _step(self, batch: Any) -> torch.Tensor:
         x, y = batch
         y0, y1, y2 = y
@@ -110,14 +91,6 @@ class YOLOv3LitModule(LightningModule):
 
     def on_train_epoch_end(self) -> None:
         self.train_loss = []
-        # current_epoch = self.current_epoch
-        # if current_epoch > 0 and (current_epoch%3 == 0 or current_epoch == self.trainer.max_epochs):
-        #     class_accuracy, no_obj_accuracy, obj_accuracy = check_class_accuracy(self, self.trainer.train_dataloader(), self.CONF_THRESHOLD)
-            
-        #     self.log("train/class_acc", class_accuracy, prog_bar=True,sync_dist=True)
-        #     self.log("train/no_obj_acc", no_obj_accuracy, prog_bar=True,sync_dist=True)
-        #     self.log("train/obj_acc", obj_accuracy, prog_bar=True,sync_dist=True)
-        # self.train_metric.reset()
 
     def validation_step(self, batch: Any, batch_idx: int) -> STEP_OUTPUT:
         loss = self._step(batch)
@@ -126,61 +99,19 @@ class YOLOv3LitModule(LightningModule):
         mean_loss = sum(self.val_loss) / len(self.val_loss)
         self.log("val/loss", mean_loss, prog_bar=True,sync_dist=True)
         
-        current_epoch = self.current_epoch
-        if current_epoch > 0 and (current_epoch%2 == 0 and batch_idx in [0, 10]):
-            plotted_image = plot_couple_examples(
-                self, batch, 0.6, 0.5, self.scaled_anchors, self.class_labels
-            )
-            for im in plotted_image:
-                self.logger.experiment.add_image(
-                    "predictions",
-                    torch.tensor(im),
-                    f"{self.current_epoch}{batch_idx}",
-                )
+        # current_epoch = self.current_epoch
+        # if current_epoch > 0 and (current_epoch%2 == 0 and batch_idx in [0, 10]):
+        #     plotted_image = plot_couple_examples(
+        #         self, batch, 0.6, 0.5, self.scaled_anchors, self.class_labels
+        #     )
+        #     for im in plotted_image:
+        #         self.logger.experiment.add_image(
+        #             "predictions",
+        #             torch.tensor(im),
+        #             f"{self.current_epoch}{batch_idx}",
+        #         )
     
         return loss
 
     def on_validation_epoch_end(self) -> None:
         self.val_loss = []
-        
-        # current_epoch = self.current_epoch
-        # loader = self.trainer.val_dataloaders()
-        
-        # if current_epoch > 0 and (current_epoch%2 == 0 or current_epoch == self.trainer.max_epochs):
-        #     class_accuracy, no_obj_accuracy, obj_accuracy = check_class_accuracy(self, loader, self.CONF_THRESHOLD)
-            
-        #     self.log("val/class_acc", class_accuracy, prog_bar=True,sync_dist=True)
-        #     self.log("val/no_obj_acc", no_obj_accuracy, prog_bar=True,sync_dist=True)
-        #     self.log("val/obj_acc", obj_accuracy, prog_bar=True,sync_dist=True)
-            
-        #     plotted_image = plot_couple_examples(
-        #         self, loader, 0.6, 0.5, self.scaled_anchors, self.class_labels
-        #     )
-        #     for idx, im in enumerate(plotted_image):
-        #         self.logger.experiment.add_image(
-        #             "sample_prediction",
-        #             torch.tensor(im),
-        #             f"{self.current_epoch}{idx}",
-        #         )
-    
-        # #if current_epoch > 0 and (current_epoch%10 == 0 or current_epoch == self.trainer.max_epochs):
-        #     # map calculation takes time, calculation after some epochs
-        #     pred_boxes, true_boxes = get_evaluation_bboxes(
-        #         loader,
-        #         self,
-        #         iou_threshold=self.NMS_IOU_THRESH,
-        #         anchors=self.ANCHORS,
-        #         threshold=self.CONF_THRESHOLD,
-        #     )
-            
-        #     mapval = mean_average_precision(
-        #         pred_boxes,
-        #         true_boxes,
-        #         iou_threshold=self.MAP_IOU_THRESH,
-        #         box_format="midpoint",
-        #         num_classes=self.NUM_CLASSES,
-        #     )
-
-        #     self.log("val/MAP", mapval, prog_bar=True ,sync_dist=True)
-
-        # # self.val_metric.reset()

582f9ae801e1aa735faab520d54374277456f932
Anant Gupta <anantgupta129@gmail.com>
2023-08-28 11:10:02 +0530

    opus dataset & lightning module

---
+++ torchood/data/components/opus_books.py
@@ -0,0 +1,75 @@
+import torch
+from torch.utils.data import Dataset
+
+
+def casual_mask(size):
+    mask = torch.triu(torch.ones(1, size, size), diagonal=1).type(torch.int)
+    return mask
+
+
+class BilingualDataset(Dataset):
+    def __init__(self, ds, seq_len, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang) -> None:
+        super().__init__()
+
+        self.ds = ds
+        self.seq_len = seq_len
+        self.tokenizer_src = tokenizer_src
+        self.tokenizer_tgt = tokenizer_tgt
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+
+        self.sos_token = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64)
+        self.eos_token = torch.tensor([tokenizer_tgt.token_to_id("[EOS]")], dtype=torch.int64)
+        self.pad_token = torch.tensor([tokenizer_tgt.token_to_id("[PAD]")], dtype=torch.int64)
+
+    def __len__(self) -> int:
+        return len(self.ds)
+
+    def __getitem__(self, index: int):
+        data = self.ds[index]
+        src = data["translation"][self.src_lang]
+        tgt = data["translation"][self.tgt_lang]
+
+        # Transform the text into tokens
+        src_ids = self.tokenizer_src.encode(src).ids  # encode input
+        tgt_ids = self.tokenizer_tgt.encode(tgt).ids  # decoder input
+
+        # add sos, eos and pad tokens
+        src_num_padding = self.seq_len - len(src_ids) - 2  # we will add <s> and </s>
+        # here only add <s>, & </s> only in the label
+        tgt_num_padding = self.seq_len - len(tgt_ids) - 1
+
+        # Make sure that pad is non negative, if it is then sentence is to long
+        if src_num_padding < 0 or tgt_num_padding < 0:
+            raise ValueError("Sentence is too long")
+
+        enoder_input = (
+            [self.sos_token] + src_ids + [self.eos_token] + [self.pad_token] * src_num_padding
+        )
+        decoder_input = [self.sos_token] * tgt_ids + [self.pad_token] * tgt_num_padding
+
+        enoder_input = torch.tensor(enoder_input, dtype=torch.int64)
+        decoder_input = torch.tensor(decoder_input, dtype=torch.int64)
+
+        # add only </s>
+        label = tgt_ids, [self.eos_token] + [self.pad_token] * tgt_num_padding
+        label = torch.tensor(label, dtype=torch.int64)
+
+        # double check the size of tensors is correct
+        assert enoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+
+        return {
+            "enoder_input": enoder_input,
+            "decoder_input": decoder_input,
+            "label": label,
+            "encoder_mask": (enoder_input != self.pad_token)
+            .unsqueeze(0)
+            .unsqueeze(0)
+            .int(),  # 1, 1, seq_len
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).int()
+            & casual_mask(decoder_input.size(0)),  # (1, seq_len) & (1, seq_len, seq_len)
+            "src_text": src,
+            "tgt_text": tgt,
+        }
---
+++ torchood/data/opus_datamodule.py
@@ -0,0 +1,112 @@
+from pathlib import Path
+from typing import Any, List, Optional, Union
+
+from datasets import load_dataset
+from lightning.pytorch import LightningDataModule
+from lightning.pytorch.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.trainers import WordLevelTrainer
+from torch.utils.data import DataLoader, Dataset, random_split
+
+from .components.opus_books import BilingualDataset
+
+
+def get_all_sentences(ds: Any, lang: str):
+    for item in ds:
+        yield item["translation"][lang]
+
+
+def make_tokenizer(config: Any, ds: Any, lang: str) -> Tokenizer:
+    tokenizer_path = Path(config["tokenizer_file"].format(lang))
+    if not tokenizer_path.is_file():
+        print(f"Tokenizer not found. Making for {lang}...")
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"), pre_tokenizer=Whitespace())
+        trainer = WordLevelTrainer(
+            special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2
+        )
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        print("Tokenizer found. Loading from file...")
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
+
+
+class OpusBooksDataModule(LightningDataModule):
+    def __init__(self, config: Any, num_workers: int = 0, pin_memory: bool = False):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+
+    def prepare_data(self) -> None:
+        # opus_books only has train split, so we will divide it later
+        load_dataset(
+            "opus_books",
+            f"{self.hparams.config['lang_src']}-{self.hparams.config['lang_tgt']}",
+            split="train",
+        )
+
+    def setup(self, stage: str) -> None:
+        # load  only if not loaded already
+        if not self.data_train and not self.data_val:
+            lang_src = self.hparams.config["lang_src"]
+            lang_tgt = self.hparams.config["lang_tgt"]
+
+            ds_raw = load_dataset("opus_books", f"{lang_src}-{lang_tgt}", split="train")
+
+            # build tokenizer
+            tokenizer_src = make_tokenizer(self.hparams.config, ds_raw, lang_src)
+            tokenizer_tgt = make_tokenizer(self.hparams.config, ds_raw, lang_tgt)
+
+            # train val split
+            train_size = int(0.9 * len(ds_raw))
+            val_size = len(ds_raw) - train_size
+            train_ds_raw, val_ds_raw = random_split(ds_raw, [train_size, val_size])
+
+            self.data_train = BilingualDataset(
+                train_ds_raw, tokenizer_src, tokenizer_tgt, lang_src, lang_tgt
+            )
+            self.data_val = BilingualDataset(
+                val_ds_raw, tokenizer_src, tokenizer_tgt, lang_src, lang_tgt
+            )
+            self.tokenizer_src = tokenizer_src
+            self.tokenizer_tgt = tokenizer_tgt
+
+            # finding max  len of each sentence in source and target dataset
+            max_len_src = 0
+            max_len_tgt = 0
+            for item in ds_raw:
+                src_ids = tokenizer_src.encode(item["translation"][lang_src]).ids
+                tgt_ids = tokenizer_tgt.encode(item["translation"][lang_tgt]).ids
+                max_len_src = max(max_len_src, len(src_ids))
+                max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+            print("[x] Maximum Length of Sentence:-")
+            print(
+                "\t[*] source sentence: %d\n\t[*] target sentence: %d" % (max_len_src, max_len_tgt)
+            )
+
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.config["batch_size"],
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=1,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )

c01fc707bdc7ae67e38dd1e65e89698a0c3bc2cf
Anant Gupta <anantgupta129@gmail.com>
2023-08-30 12:37:58 +0530

    mask size fixed

--- torchood/data/components/opus_books.py
+++ torchood/data/components/opus_books.py
@@ -3,7 +3,7 @@ from torch.utils.data import Dataset
 
 
 def casual_mask(size):
-    mask = torch.triu(torch.ones(1, size, size), diagonal=1).type(torch.int)
+    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
     return mask
 
 

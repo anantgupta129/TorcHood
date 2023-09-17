f52930c6742a15afcadbd18ca9e12a4d9c2db396
Anant Gupta <anantgupta129@gmail.com>
2023-09-17 11:28:28 +0530

    added strided convolution / pooling arch UNet

--- torchood/models/components/unet.py
+++ torchood/models/components/unet.py
@@ -15,14 +15,14 @@ import torchvision.transforms.functional as F
 
 
 class ContractingBlock(nn.Module):
-    """Defines a block for the contracting path.
+    """Defines a block for the contracting path. Encoder Block.
 
     Attributes:
         layer1 (torch.nn.Sequential): First part of the contracting block.
         layer2 (torch.nn.Sequential): Second part of the contracting block.
     """
 
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(self, in_channels: int, out_channels: int, strided_conv: bool = False):
         super().__init__()
 
         # Two convolution layers followed by batch normalization and ReLU activation
@@ -31,21 +31,40 @@ class ContractingBlock(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-        )
+        if strided_conv:
+            self.layer2 = nn.Sequential(
+                nn.Conv2d(
+                    out_channels, out_channels, kernel_size=3, padding=1, stride=2, bias=False
+                ),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(),
+            )
+        else:
+            self.layer2 = nn.Sequential(
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(),
+            )
+        # Max pooling layer
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.strided_conv = strided_conv
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Passes the input tensor through the block layers."""
         x = self.layer1(x)
-        x = self.layer2(x)
-        return x
+        if self.strided_conv:
+            skip = x
+            x = self.layer2(x)
+        else:
+            x = self.layer2(x)
+            skip = x
+            x = self.maxpool(x)
+
+        return x, skip
 
 
 class ExpandingBlock(nn.Module):
-    """Defines a block for the expanding path.
+    """Defines a block for the expanding path. Decoder block.
 
     Attributes:
         conv1, conv2 (torch.nn.Conv2d): Convolutional layers.
@@ -76,6 +95,7 @@ class ExpandingBlock(nn.Module):
         x = self.upsample(x)
         # Resize x to match the spatial dimensions of skip tensor
         if x.shape[2] != skip.shape[2]:
+            print("did - ", x.shape, skip.shape)
             x = F.resize(x, size=skip.shape[2:])
         x = torch.cat((x, skip), dim=1)
         x = self.conv1(x)
@@ -94,17 +114,17 @@ class UNet(nn.Module):
     contracting path captures context, and the expansive path enables precise localization.
     """
 
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(self, in_channels: int, out_channels: int, strided_conv: bool = False):
         super().__init__()
 
-        # Contracting Path
-        self.contract1 = ContractingBlock(in_channels, 64)
-        self.contract2 = ContractingBlock(64, 128)
-        self.contract3 = ContractingBlock(128, 256)
-        self.contract4 = ContractingBlock(256, 512)
+        # Contracting Path (Encoder)
+        self.contract1 = ContractingBlock(in_channels, 64, strided_conv)
+        self.contract2 = ContractingBlock(64, 128, strided_conv)
+        self.contract3 = ContractingBlock(128, 256, strided_conv)
+        self.contract4 = ContractingBlock(256, 512, strided_conv)
         self.contract5 = ContractingBlock(512, 1024)
 
-        # Expanding Path
+        # Expanding Path (Decoder)
         self.expand1 = ExpandingBlock(1024, 512)
         self.expand2 = ExpandingBlock(512, 256)
         self.expand3 = ExpandingBlock(256, 128)
@@ -112,42 +132,36 @@ class UNet(nn.Module):
 
         # Final convolution to map to the desired number of classes
         self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1, bias=False)
-        # Max pooling layer
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Passes the input tensor through the U-Net model and returns the segmentation map."""
         # Contracting path
-        x1 = self.contract1(x)
-        x2 = self.maxpool(x1)
-        x3 = self.contract2(x2)
-        x4 = self.maxpool(x3)
-        x5 = self.contract3(x4)
-        x6 = self.maxpool(x5)
-        x7 = self.contract4(x6)
-        x8 = self.maxpool(x7)
-        x9 = self.contract5(x8)
+        x, skip1 = self.contract1(x)
+        x, skip2 = self.contract2(x)
+        x, skip3 = self.contract3(x)
+        x, skip4 = self.contract4(x)
+        _, x = self.contract5(x)  # in last layer pooling / strides is not required
 
         # Expanding path with skip connections
-        x = self.expand1(x9, x7)
-        x = self.expand2(x, x5)
-        x = self.expand3(x, x3)
-        x = self.expand4(x, x1)
+        x = self.expand1(x, skip4)
+        x = self.expand2(x, skip3)
+        x = self.expand3(x, skip2)
+        x = self.expand4(x, skip1)
 
         # Final convolution layer
         x = self.final_conv(x)
         return x
 
 
-if __name__ == "__main__":
-    from torchinfo import summary
+# if __name__ == "__main__":
+#     from torchinfo import summary
 
-    # Creating a U-Net model
-    model = UNet(3, 10)
-    input_size = (1, 3, 576, 576)
-    x = torch.randn(*input_size)
+#     # Creating a U-Net model
+#     model = UNet(3, 10, strided_conv=True)
+#     input_size = (1, 3, 256, 256)
+#     x = torch.randn(*input_size)
 
-    out = model(x)  # dry run
-    print(out.shape)
-    # Printing the model summary
-    summary(model, input_size=input_size, device="cpu")
+#     out = model(x)  # dry run
+#     print(out.shape)
+#     # Printing the model summary
+#     summary(model, input_size=input_size, device="cpu")
